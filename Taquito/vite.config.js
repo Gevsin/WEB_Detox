220722c8f4f8df7460434e83e0433a4be72b54c2
Adidem23 <adityasuryawanshi5451@gmail.com>
2024-02-09 15:56:35 +0530

    Added Taquito and Added TezosNft Project

--- Taquito/vite.config.js
+++ Taquito/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), NodeGlobalsPolyfillPlugin({ Buffer: true })],
+  plugins: [react()],
   define: {
     global: {
       Buffer:{}
